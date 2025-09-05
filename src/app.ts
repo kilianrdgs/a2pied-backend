8452327e80507ca0c7c789518f412ce6d2494be1
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-05 12:09:30 +0200

    quick update

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import swaggerUi from "swagger-ui-express";
 
 import "dotenv/config";
 import cors from "cors";
-import { connectMongo } from "./db/mongo.js";
+import {connectMongo} from "./db/mongo.js";
 import swaggerOptions from "./docs/swagger.js";
 import globalRouter from "./router.js";
 
@@ -16,26 +16,30 @@ const specs = swaggerJsdoc(swaggerOptions);
 app.use(express.json());
 
 app.use(
-	cors({
-		origin: ["*"],
-		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-	}),
+    cors({
+        origin: ["*"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
 );
+
+app.get('/', (req, res) => {
+    res.send('ok');
+});
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api", globalRouter);
 
 connectMongo()
-	.then(() => {
-		app.listen(PORT, () => {
-			console.log(`🚀 API running at http://localhost:${PORT}`);
-			console.log("--------------------------------");
-			console.log(`📖 Swagger UI at http://localhost:${PORT}/doc`);
-			console.log("--------------------------------");
-		});
-	})
-	.catch((e) => {
-		console.error("❌ Failed to start server:", e);
-		process.exit(1);
-	});
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`🚀 API running at http://localhost:${PORT}`);
+            console.log("--------------------------------");
+            console.log(`📖 Swagger UI at http://localhost:${PORT}/doc`);
+            console.log("--------------------------------");
+        });
+    })
+    .catch((e) => {
+        console.error("❌ Failed to start server:", e);
+        process.exit(1);
+    });
