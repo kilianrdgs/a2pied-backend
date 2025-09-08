6a987c83dc9ec14717da0e5b7331fedd1a6a6ce8
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-08 10:27:44 +0200

    ajout du router mobType

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
-import { Router } from "express";
+import {Router} from "express";
 import userRouter from "./users/router.js";
+import mobTypesRouter from "./mobTypes/mobTypes.router.js";
 
 const globalRouter = Router();
 
 globalRouter.use("/users", userRouter);
+globalRouter.use("/mobTypes", mobTypesRouter);
 
 export default globalRouter;
