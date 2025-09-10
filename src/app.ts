48a07ec4321bb3c9813b4b92a23a32a135d86fdc
kilianrdgs <krdgs.rodrigues@gmail.com>
2025-09-10 11:19:13 +0200

    clefapi

--- src/app.ts
+++ src/app.ts
@@ -3,49 +3,48 @@ import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import "dotenv/config";
 import cors from "cors";
-import {connectMongoose} from "./db/mongo.js";
+import { connectMongoose } from "./db/mongo.js";
 import swaggerOptions from "./docs/swagger.js";
 import globalRouter from "./router.js";
-import {createServer} from "node:http";
-import {webSocketSetup} from "./websocket/utils/webSocketSetup.js";
-
+import { createServer } from "node:http";
+import { webSocketSetup } from "./websocket/utils/webSocketSetup.js";
+import { apiKeyMiddleware } from "./middlewares/apiKey.js";
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT;
-const FRONT_URL = process.env.FRONT_URL ?? ""
+const FRONT_URL = process.env.FRONT_URL ?? "";
 
 const specs = swaggerJsdoc(swaggerOptions);
 
 app.use(express.json());
 
 app.use(
-    cors({
-        origin: ["*", FRONT_URL],
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    }),
+  cors({
+    origin: ["*", FRONT_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 app.get("/", (req, res) => {
-    res.send("ok");
+  res.send("ok");
 });
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(specs));
-app.use("/api", globalRouter);
+app.use("/api", apiKeyMiddleware, globalRouter);
 
-webSocketSetup(server)
+webSocketSetup(server);
 connectMongoose()
-    .then(() => {
-        server.listen(PORT, () => {
-            console.log(`🚀 API running at http://localhost:${PORT}`);
-            console.log("--------------------------------");
-            console.log(`📖 Swagger UI at http://localhost:${PORT}/doc`);
-            console.log("--------------------------------");
-        });
-    })
-    .catch((e) => {
-        console.error("❌ Failed to start server:", e);
-        process.exit(1);
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`🚀 API running at http://localhost:${PORT}`);
+      console.log("--------------------------------");
+      console.log(`📖 Swagger UI at http://localhost:${PORT}/doc`);
+      console.log("--------------------------------");
     });
-
+  })
+  .catch((e) => {
+    console.error("❌ Failed to start server:", e);
+    process.exit(1);
+  });
