d61b8edc6ea46a91591f1e7854e2288476b1f6eb
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-18 15:14:37 +0200

    reset des credits lors d'un reset d'un jeu

--- src/upgrade/service/deleteAllUpgrades.service.ts
+++ src/upgrade/service/deleteAllUpgrades.service.ts
@@ -1,5 +1,7 @@
 import {UpgradeModel} from "../entities/upgrade.model.js";
+import {UserModel} from "../../users/entities/user.model.js";
 
 export async function deleteAllUpgradesService() {
+    await UserModel.updateMany({}, {upgrades: []})
     return UpgradeModel.deleteMany();
 }
--- src/websocket/service/handleResetGameStateEvent.service.ts
+++ src/websocket/service/handleResetGameStateEvent.service.ts
@@ -1,7 +1,9 @@
 import {resetGameState} from "../../game/gameState.js";
 import {creditsService} from "../../credits/service/credits.service.js";
+import {deleteAllUpgradesService} from "../../upgrade/service/deleteAllUpgrades.service.js";
 
 export async function handleResetGameStateEventService() {
     resetGameState()
+    await deleteAllUpgradesService()
     await creditsService.resetAllCredits()
 }
