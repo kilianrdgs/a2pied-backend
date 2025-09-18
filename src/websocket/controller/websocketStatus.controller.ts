6735d3c061eeba5b38ba2dcbdce23bb4abdc7183
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-18 15:55:46 +0200

    broadcast d'un event lors du reset

--- src/websocket/controller/websocketStatus.controller.ts
+++ src/websocket/controller/websocketStatus.controller.ts
@@ -3,6 +3,9 @@ import {godotWs, websocketClients} from "../type/websocketState.js";
 import {getGameState, resetGameState} from "../../game/gameState.js";
 import {deleteAllUpgradesService} from "../../upgrade/service/deleteAllUpgrades.service.js";
 import {creditsService} from "../../credits/service/credits.service.js";
+import {WebsocketCommunicationS2CType, WebsocketEventS2CEnum} from "../type/WebsocketCommunicationS2CType.js";
+import {WebsocketEventC2SEnum} from "../type/WebsocketCommunicationC2SType.js";
+import {broadCastJSONMessage} from "../utils/sendWebsocketJSONMessage.js";
 
 export async function getWebsocketStatusData(_req: Request, res: Response) {
     const ws_clients_mail: string[] = []
@@ -25,6 +28,18 @@ export async function getWebsocketStatusData(_req: Request, res: Response) {
 export async function resetGameStateController(_req: Request, res: Response) {
     resetGameState()
     await deleteAllUpgradesService()
+    console.log('upodate')
     await creditsService.resetAllCredits()
+    console.log("all credit reset")
+    const broadcastMessage: WebsocketCommunicationS2CType = {event: WebsocketEventS2CEnum.BROADCAST}
+
+    broadcastMessage.data = {
+        event: WebsocketEventC2SEnum.UPDATE_STATE,
+        data: {action: "reset"},
+        timestamp: new Date().getTime().toString()
+    }
+
+    await broadCastJSONMessage(broadcastMessage);
+
     res.status(200).send("OK");
 }
